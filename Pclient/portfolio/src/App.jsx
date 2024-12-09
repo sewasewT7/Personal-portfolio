e493eb20ed9dab5b912e67ef699b14a07ccf5966
Sewasew <sewasewtadele@gmail.com>
2024-12-09 16:56:07 +0300

    #commit

--- Pclient/portfolio/src/App.jsx
+++ Pclient/portfolio/src/App.jsx
@@ -1,4 +1,17 @@
-import "./App.css";
-function App() {}
+import {
+  BrowserRouter as Router,
+  // Route,
+  // Routes,
+  // Navigate,
+} from "react-router-dom";
+import Header from "./Header.jsx";
+
+function App() {
+  return (
+    <Router>
+      <Header />
+    </Router>
+  );
+}
 
 export default App;
---
+++ Pclient/portfolio/src/Test.jsx
@@ -0,0 +1,14 @@
+function Test() {
+  return (
+    <>
+      <h1>Hello there</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Eaque, ut
+        reiciendis consequatur repudiandae quos doloribus quo quaerat saepe
+        labore dolores alias, error recusandae necessitatibus eligendi,
+        voluptatibus aliquid sed soluta placeat.
+      </p>
+    </>
+  );
+}
+export default Test;
