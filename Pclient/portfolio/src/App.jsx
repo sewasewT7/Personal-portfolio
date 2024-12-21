f9ba982ec04c18a5296da829566f634d3e199f9c
Sewasew <sewasewtadele@gmail.com>
2024-12-21 16:33:12 +0300

    About page updated

--- Pclient/portfolio/src/App.jsx
+++ Pclient/portfolio/src/App.jsx
@@ -7,7 +7,8 @@ import {
 import Hom from "./Hom.jsx";
 import Header from "./Header.jsx";
 import About from "./About.jsx";
-
+import Skills from "./Skills.jsx";
+import Contact from "./Contact.jsx";
 function App() {
   return (
     <Router>
@@ -18,6 +19,8 @@ function App() {
           <Route path="/" element={<Navigate to="/home" />} />
           <Route path="/home" element={<Hom />} />
           <Route path="/about" element={<About />} />
+          <Route path="/skills" element={<Skills />} />
+          <Route path="/contact" element={<Contact />} />
         </Routes>
       </main>
     </Router>
